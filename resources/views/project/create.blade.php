fbafd630ce9d3f8c5a03c0c80d3ec2daaa3af9ca
Kalal mukesh <105076417+Mukeshkalal@users.noreply.github.com>
2024-05-22 11:18:19 +0530

    comment remuve in create page

--- resources/views/project/create.blade.php
+++ resources/views/project/create.blade.php
@@ -5,20 +5,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Create</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="container my-2">
-<<<<<<< Updated upstream:resources/views/project/create.blabe.php
-        <form action="{{route('project.store')}}" method="post">
-=======
-        <h2 class="text-center border-bottom pb-2 border-5 border-info">Project Create</h2>
-        {{-- @foreach ($error->all() as $err)
-            {{ $err }}
-        @endforeach --}}
         <form action="{{ route('project.store') }}" method="post">
->>>>>>> Stashed changes:resources/views/project/create.blade.php
+            <h2 class="text-center border-bottom pb-2 border-5 border-info">Project Create</h2>
+
             @csrf
             <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
@@ -26,7 +21,8 @@
             </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" />
+                <input type="email" class="form-control" id="email" name="email"
+                    placeholder="name@example.com" />
             </div>
             <div class="mb-3">
                 <label for="contact" class="form-label">number</label>
@@ -45,7 +41,9 @@
             </div>
         </form>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
