3c18c145d6883664adab3c97a5f8b14899c4cee6
Kalal mukesh <105076417+Mukeshkalal@users.noreply.github.com>
2024-07-04 15:42:05 +0530

    add clear the data

--- resources/views/project/create.blade.php
+++ resources/views/project/create.blade.php
@@ -1,14 +1,10 @@
 @extends('layouts.app')
-@section('title', 'Project Create Page ')
+@section('title', 'Project Create Page')
 @section('content')
 
     <div class="container my-2">
-<<<<<<< HEAD
         <h2 class="text-center border-bottom pb-2 border-5 border-info">Project Create</h2>
-=======
->>>>>>> c5fd5b2961a688ce030e0aefdcb2b95d2641e686
         <form action="{{ route('project.store') }}" method="post">
-            <h2 class="text-center border-bottom pb-2 border-5 border-info">Project Create</h2>
             @csrf
             <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
@@ -20,15 +16,11 @@
             </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-<<<<<<< HEAD
                 <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" />
-=======
-                <input type="email" class="form-control" id="email" name="email"
-                    placeholder="name@example.com" />
                 @if ($errors->has('email'))
                     <span class="text-danger">{{ $errors->first('email') }}</span>
                 @endif
->>>>>>> c5fd5b2961a688ce030e0aefdcb2b95d2641e686
+
             </div>
             <div class="mb-3">
                 <label for="contact" class="form-label">number</label>
@@ -56,13 +48,5 @@
             </div>
         </form>
     </div>
-<<<<<<< HEAD
-@endsection
-=======
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-</body>
 
-</html>
->>>>>>> c5fd5b2961a688ce030e0aefdcb2b95d2641e686
+@endsection
